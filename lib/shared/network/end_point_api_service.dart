f135340f5b5c6096c0ce09ad87e84ead6f406236
LENOVO <mohamed209059@gmail.com>
2022-08-24 21:34:54 +0200

    Initial commit

---
+++ lib/shared/network/end_point_api_service.dart
@@ -0,0 +1,32 @@
+
+class EndPointApiService {
+
+  //auth service
+  static String loginPath = '';
+  static String registerPath = '';
+  static String forgetPasswordPath = '';
+
+  //user service
+  static String homePath = '';
+  static String newOrderUserPath = '';
+  static String currentOrderUserPath = '';
+  static String previousOrderUserPath = '';
+  static String profileUserPath = '';
+  static String updateProfileUserPath = '';
+  static String enterServiceRequestUserPath = '';
+  static String rateUserToTechnicalUserPath = '';
+
+  //technical service
+  static String newOrderTechnicalPath = '';
+  static String currentOrderTechnicalPath = '';
+  static String previousOrderTechnicalPath = '';
+  static String profileTechnicalPath = '';
+  static String updateProfileTechnicalPath = '';
+  static String enterServiceMoneyFromTechnical = '';
+
+  // services
+  static String aboutUsPath = '';
+  static String privacyPath = '';
+  static String termsAndConditionalPath = '';
+
+}
